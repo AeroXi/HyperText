70ee7a936c00ef7f363337d26a5f22c720fcbd9a
aeroxi <magictalentx@gmail.com>
2023-04-30 16:31:58 +0800

    change import alias to @

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import SearchBar from '@/components/SearchBar';
 
 const Home: NextPage = () => {
   return (<>
   <p>Hello, CT3A!</p>
+  <SearchBar />
   </>)
 }
 
